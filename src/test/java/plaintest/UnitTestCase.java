3fee4b3b9f2eb83366b076f3cdc04ecc836f64da
KrushnaAgale <agalekrushnab@gmail.com>
2022-07-13 15:21:15 +0530

    From Krushna

---
+++ src/test/java/plaintest/UnitTestCase.java
@@ -0,0 +1,10 @@
+package plaintest;
+
+public class UnitTestCase {
+public static void main(String[]args)
+{
+	System.setProperty("webdriver.chrome.driver", "D:\\MyNewProject\\Flipkart\\src\\test\\resources\\DRIVERS\\chromedriver.exe");
+	ChromeDriver driver=new ChromeDriver();
+	
+}
+}
